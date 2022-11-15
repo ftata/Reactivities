616de06c02634d73d3134a6d8a87dd40ddbc2085
frankt <frankt@matrixcontrols.com>
2022-11-14 21:28:43 -0500

    End of Section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -3,6 +3,8 @@ import React, { useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
 import { useStore } from "../../../stores/store";
+import ActivityFilters from "./ActivityFilters";
+
 import ActivityList from "./ActivityList";
 
 
@@ -24,7 +26,7 @@ export default observer(function ActivityDashboard() {
         <ActivityList />
       </Grid.Column>
       <Grid.Column width="6">
-        <h2>Activity Filters</h2>
+       <ActivityFilters />
       </Grid.Column>
     </Grid>
   );
